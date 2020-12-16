0c3b940c3747be05db83745fad4924999737f279
Ranulf Jonathan Noronha <69053657+ranulfjn@users.noreply.github.com>
2020-12-16 13:20:56 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import SearchPhotos from './search'
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="container" >
+       <h1>
+         Search For Photos 
+       </h1>
+        <SearchPhotos/>
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/search.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+export default function SearchPhotos() {
+
+  const [client,setClient]=useState("1ZWepNv7KJ9A5sr1Gqkq3eP1vvvSUTEG7jyyz5Nkk8k");
+  const [query, setQuery] = useState("");
+  const [photos, setPhotos] = useState([]);
+
+
+  const searchPhotos = (e) => {
+    e.preventDefault();
+
+    const url="https://api.unsplash.com/search/photos?query="+query+"&per_page=20&client_id="+client;
+    fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+     
+      setPhotos(result);
+      console.log(result);
+    });
+
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchPhotos}>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Search...`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+
+      <div className="card-list">
+        {[photos].map((photo,index)=>{
+          // console.log(photo);
+          // console.log(photo.results[index].urls.full);
+          <div className="card">
+          <img
+          className="card--image"
+          alt=""
+          src={photo.results[index].urls.full}
+        ></img>
+          </div>
+          })}
+      </div>
+    </>
+  );
+}
